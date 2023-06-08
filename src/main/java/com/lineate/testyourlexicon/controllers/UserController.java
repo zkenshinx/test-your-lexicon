a79d3dad4d7bbbc978cc833f84f37dd7c70431d1
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-06-08 17:32:53 +0400

    Implemented registration user endpoint

--- src/main/java/com/lineate/testyourlexicon/controllers/UserController.java
+++ src/main/java/com/lineate/testyourlexicon/controllers/UserController.java
@@ -1,11 +1,11 @@
 package com.lineate.testyourlexicon.controllers;
 
 import com.lineate.testyourlexicon.dto.UserRegistrationDto;
-import com.lineate.testyourlexicon.services.UserRegistrationResult;
-import com.lineate.testyourlexicon.services.UserService;
+import com.lineate.testyourlexicon.services.implementations.UserServiceImpl;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class UserController {
 
-  private final UserService userService;
+  private final UserServiceImpl userService;
 
   @PostMapping("/register")
-  public ResponseEntity<String> registerUser(@RequestBody @Valid UserRegistrationDto userRegistrationDto) {
-    UserRegistrationResult userRegistrationResult = userService.createUser(userRegistrationDto);
-    if (userRegistrationResult.isSuccessfulRegistration())
-      return ResponseEntity.ok(userRegistrationResult.getMessage());
-    return ResponseEntity.badRequest().body(userRegistrationResult.getMessage());
+  public ResponseEntity<Object> registerUser(@RequestBody @Valid UserRegistrationDto userRegistrationDto) {
+    var userRegistrationResponse = userService.createUser(userRegistrationDto);
+    if (userRegistrationResponse.isSuccessfulRegistration()) {
+      return ResponseEntity.ok(userRegistrationResponse.getMessage());
+    }
+    return ResponseEntity.badRequest().body(userRegistrationResponse.getMessage());
   }
 
 }
