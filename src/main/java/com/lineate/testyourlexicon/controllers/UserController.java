ceebbae8883cb5dc1beb2a615242c1619e6799fc
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-06-09 19:07:23 +0400

    Removed single UserService Interface

--- src/main/java/com/lineate/testyourlexicon/controllers/UserController.java
+++ src/main/java/com/lineate/testyourlexicon/controllers/UserController.java
@@ -1,22 +1,21 @@
 package com.lineate.testyourlexicon.controllers;
 
 import com.lineate.testyourlexicon.dto.UserRegistrationDto;
-import com.lineate.testyourlexicon.services.implementations.UserServiceImpl;
+import com.lineate.testyourlexicon.services.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
 
-  private final UserServiceImpl userService;
+  private final UserService userService;
 
   @PostMapping("/register")
   public ResponseEntity<Object> registerUser(@RequestBody @Valid UserRegistrationDto userRegistrationDto) {
--- src/main/java/com/lineate/testyourlexicon/services/UserService.java
+++ src/main/java/com/lineate/testyourlexicon/services/UserService.java
@@ -1,9 +1,49 @@
 package com.lineate.testyourlexicon.services;
 
 import com.lineate.testyourlexicon.dto.UserRegistrationDto;
+import com.lineate.testyourlexicon.entities.User;
+import com.lineate.testyourlexicon.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
+@Service
+@AllArgsConstructor
+@Slf4j
+public class UserService {
 
-  public UserRegistrationResponse createUser(UserRegistrationDto userRegistrationDto);
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  private void logUserRegistration(UserRegistrationDto userRegistrationDto) {
+    log.info("Registered user: {first name: {}, last name: {}, email: {}}",
+      userRegistrationDto.getFirstName().toLowerCase(),
+      userRegistrationDto.getLastName().toLowerCase(),
+      userRegistrationDto.getEmail());
+  }
+
+  public UserRegistrationResponse createUser(UserRegistrationDto userRegistrationDto) {
+    if (userRepository.findUserByEmail(userRegistrationDto.getEmail()).isPresent()) {
+      return new UserRegistrationResponse(false, "User with the given mail exists");
+    } else if (!userRegistrationDto.getPassword().equals(userRegistrationDto.getConfirmationPassword())) {
+      return new UserRegistrationResponse(false, "Passwords do not match");
+    }
+
+    User registeredUser = new User();
+    registeredUser.setFirstName(userRegistrationDto.getFirstName().toLowerCase());
+    registeredUser.setLastName(userRegistrationDto.getLastName().toLowerCase());
+    registeredUser.setEmail(userRegistrationDto.getEmail());
+    String hashedPassword = passwordEncoder.encode(userRegistrationDto.getPassword());
+    registeredUser.setHashedPassword(hashedPassword);
+
+    userRepository.save(registeredUser);
+
+    logUserRegistration(userRegistrationDto);
+
+    return new UserRegistrationResponse(true, "Successful registration");
+  }
 
 }
