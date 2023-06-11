ad48ccdc69672b5e4057678775f486ad6f8ca018
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-06-11 21:33:45 +0400

    Added roles in java

--- src/main/java/com/lineate/testyourlexicon/entities/User.java
+++ src/main/java/com/lineate/testyourlexicon/entities/User.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -21,5 +24,13 @@ public class User {
   private String email;
   @Column(name = "password")
   private String hashedPassword;
+
+  @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+  @JoinTable(
+    name = "user_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id")
+  )
+  private Set<Role> roles = new HashSet<>();
 
 }
