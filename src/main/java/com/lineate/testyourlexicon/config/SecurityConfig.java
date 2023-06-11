e7780c6d6ec9c9671b0c3776c6cc888ae95ac42c
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-06-11 15:33:59 +0400

    Added Basic Authentication Mechanism

--- src/main/java/com/lineate/testyourlexicon/config/SecurityConfig.java
+++ src/main/java/com/lineate/testyourlexicon/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.lineate.testyourlexicon.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -13,7 +16,10 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
+      .authorizeHttpRequests(authz -> authz
+        .requestMatchers("/health").authenticated()
+        .anyRequest().permitAll())
+      .httpBasic(withDefaults())
       .build();
   }
 
