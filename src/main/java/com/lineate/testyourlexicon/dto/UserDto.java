d35a9f0eea50bf15d05fcf09d98fa3fa47ed1e06
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-06-13 09:03:36 +0400

    Changed UserDto to record

--- src/main/java/com/lineate/testyourlexicon/dto/UserDto.java
+++ src/main/java/com/lineate/testyourlexicon/dto/UserDto.java
@@ -1,19 +1,10 @@
 package com.lineate.testyourlexicon.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.HttpStatus;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class UserDto {
-
-  private final int code = HttpStatus.CREATED.value();
-  private String firstName;
-  private String lastName;
-  private String email;
-
+public record UserDto(String firstName, String lastName, String email) {
+  public UserDto(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
 
 }
