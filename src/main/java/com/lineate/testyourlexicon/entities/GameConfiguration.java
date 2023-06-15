fd2c71d05966ea8e917246be1a7a56767c452c36
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-06-15 13:39:49 +0400

    Added configuration entity

---
+++ src/main/java/com/lineate/testyourlexicon/entities/GameConfiguration.java
@@ -0,0 +1,33 @@
+package com.lineate.testyourlexicon.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import lombok.*;
+
+@Entity
+@Table(name = "game_configuration")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GameConfiguration {
+
+  @Id
+  @Column(name = "user_id")
+  private Long id;
+  @Column(name = "translate_from")
+  private String translateFrom;
+  @Column(name = "translate_to")
+  private String translateTo;
+  @Column(name = "number_of_steps")
+  private int numberOfSteps;
+  @Column(name = "step_time")
+  private int stepTimeInSeconds;
+
+  @OneToOne
+  @MapsId
+  @JoinColumn(name = "user_id")
+  private User user;
+}
