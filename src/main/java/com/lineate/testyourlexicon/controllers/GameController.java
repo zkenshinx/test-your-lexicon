c7c8e531034adf24a0ed52cb5ae48d795ecd513b
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-07-04 19:16:20 +0400

    Moved userHash logic from controller to service

--- src/main/java/com/lineate/testyourlexicon/controllers/GameController.java
+++ src/main/java/com/lineate/testyourlexicon/controllers/GameController.java
@@ -4,7 +4,6 @@ import com.lineate.testyourlexicon.dto.*;
 import com.lineate.testyourlexicon.services.AuthenticationService;
 import com.lineate.testyourlexicon.services.GameService;
 import com.lineate.testyourlexicon.services.UserService;
-import com.lineate.testyourlexicon.util.Hash;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,25 +16,24 @@ import org.springframework.web.bind.annotation.*;
 public class GameController {
 
   private final GameService gameService;
-  private final UserService userService;
   private final AuthenticationService authenticationService;
 
   @PostMapping
   public GameInitializedDto startGame(HttpServletRequest request) {
-    return gameService.initGameForUser(getUserHash(request));
+    return gameService.initGameForUser(authenticationService.getUserHash(request));
   }
 
   @GetMapping("/{gameId}/step")
   public StepDto nextStep(@PathVariable("gameId") Long gameId,
                           HttpServletRequest request) {
-    return gameService.userActiveGameNextStep(getUserHash(request), gameId);
+    return gameService.userActiveGameNextStep(authenticationService.getUserHash(request), gameId);
   }
 
   @PutMapping("/{gameId}/answer")
   public AnswerResponseDto answer(@PathVariable("gameId") Long gameId,
                                   @RequestBody AnswerRequestDto answerRequestDto,
                                   HttpServletRequest request) {
-    return gameService.userActiveGameAnswer(getUserHash(request), answerRequestDto, gameId);
+    return gameService.userActiveGameAnswer(authenticationService.getUserHash(request), answerRequestDto, gameId);
   }
 
   @GetMapping("/supported-languages")
@@ -47,20 +45,12 @@ public class GameController {
   public GameConfigurationDto configure(
       @RequestBody @Valid GameConfigurationDto gameConfigurationDto,
       HttpServletRequest request) {
-    return gameService.configure(gameConfigurationDto, getUserHash(request));
+    return gameService.configure(gameConfigurationDto, authenticationService.getUserHash(request));
   }
 
   @GetMapping("/configuration")
   public GameConfigurationDto configuration(HttpServletRequest request) {
-    return gameService.userConfiguration(getUserHash(request));
+    return gameService.userConfiguration(authenticationService.getUserHash(request));
   }
 
-  private Long getUserHash(HttpServletRequest request) {
-    if (authenticationService.isAuthenticated()) {
-      Long id = userService
-          .findUserByEmail(authenticationService.getAuthenticatedUserEmail()).getId();
-      return Hash.hashToLong(id);
-    }
-    return Hash.hashToLong(request.getSession().getId());
-  }
 }
--- src/main/java/com/lineate/testyourlexicon/services/AuthenticationService.java
+++ src/main/java/com/lineate/testyourlexicon/services/AuthenticationService.java
@@ -3,6 +3,9 @@ package com.lineate.testyourlexicon.services;
 import com.lineate.testyourlexicon.entities.User;
 import com.lineate.testyourlexicon.repositories.UserRepository;
 import java.util.Optional;
+
+import com.lineate.testyourlexicon.util.Hash;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -29,5 +32,14 @@ public class AuthenticationService {
   public Optional<User> getAuthenticatedUser() {
     String email = this.getAuthenticatedUserEmail();
     return userRepository.findUserByEmail(email);
+  }
+
+  public Long getUserHash(HttpServletRequest request) {
+    if (isAuthenticated()) {
+      Long id = userRepository.findUserByEmail(getAuthenticatedUserEmail())
+        .get().getId();
+      return Hash.hashToLong(id);
+    }
+    return Hash.hashToLong(request.getSession().getId());
   }
 }
