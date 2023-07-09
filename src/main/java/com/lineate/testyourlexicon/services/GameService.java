3491b53a87004f02b82c0ce1d0ee3b1728c0c3ae
Koba Karaputadze <kobikokaraputadze@gmail.com>
2023-07-09 12:47:46 +0400

    Added tests

--- src/main/java/com/lineate/testyourlexicon/services/GameService.java
+++ src/main/java/com/lineate/testyourlexicon/services/GameService.java
@@ -98,6 +98,7 @@ public class GameService {
 
   public void startStepTimeout(long gameId, int time) {
     String redisKey = String.format("game_id:step:%d", gameId);
+    System.out.println(jedis);
     jedis.setex(redisKey, time, "");
   }
 
